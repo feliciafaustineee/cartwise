cc40af03faff316bb165f84e7d9d606c516e7cdb
feliciaa17 <feliciafaustineeh@gmail.com>
2026-05-15 00:56:31 +0700

    connect db, complete be sign up and sign in

---
+++ BackEnd/src/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+
+const pool = require("./config/db");
+
+const app = express();
+
+const authRoutes = require("./routes/authRoutes");
+const authMiddleware = require("./middleware/authMiddleware");
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+
+    res.json({
+      message: "CartWise API running",
+      time: result.rows[0],
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      error: "Database connection failed",
+    });
+  }
+});
+
+app.get("/api/protected", authMiddleware, (req, res) => {
+  res.json({
+    message: "Protected route accessed",
+    user: req.user,
+  });
+});
+
+module.exports = app;
---
+++ BackEnd/src/controllers/authController.js
@@ -0,0 +1,141 @@
+const pool = require("../config/db");
+const bcrypt = require("bcryptjs");
+
+const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        error: "All fields are required",
+      });
+    }
+
+    if (name.length < 2) {
+      return res.status(400).json({
+        error: "Name must be at least 2 characters",
+      });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({
+        error: "Invalid email format",
+      });
+    }
+
+    const passwordRegex =
+      /^(?=.*[A-Z])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,12}$/;
+
+    if (!passwordRegex.test(password)) {
+      return res.status(400).json({
+        error:
+          "Password must be 8-12 characters, contain 1 uppercase letter and 1 special character",
+      });
+    }
+
+    const existingUser = await pool.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+
+    if (existingUser.rows.length > 0) {
+      return res.status(400).json({
+        error: "Email already registered",
+      });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = await pool.query(
+      `
+      INSERT INTO users (name, email, password_hash)
+      VALUES ($1, $2, $3)
+      RETURNING id, name, email
+      `,
+      [name, email, passwordHash]
+    );
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: newUser.rows[0],
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      error: "Server error",
+    });
+  }
+};
+
+const jwt = require("jsonwebtoken");
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        error: "All fields are required",
+      });
+    }
+
+    const user = await pool.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+
+    if (user.rows.length === 0) {
+      return res.status(400).json({
+        error: "Invalid email or password",
+      });
+    }
+
+    const validPassword = await bcrypt.compare(
+      password,
+      user.rows[0].password_hash
+    );
+
+    if (!validPassword) {
+      return res.status(400).json({
+        error: "Invalid email or password",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user.rows[0].id,
+        email: user.rows[0].email,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user.rows[0].id,
+        name: user.rows[0].name,
+        email: user.rows[0].email,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      error: "Server error",
+    });
+  }
+};
+
+module.exports = {
+  register,
+  login,
+};
