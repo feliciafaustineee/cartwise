623b5c2d8a9044d3f30c4675e0c82310b4e2651e
feliciaa17 <feliciafaustineeh@gmail.com>
2026-06-01 16:11:38 +0700

    fixed case senstive imports

--- FrontEnd/src/pages/Notification.jsx
+++ FrontEnd/src/pages/Notification.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
-import Sidebar from "../components/Sidebar";
+import Sidebar from "../components/sidebar";
 import {
   MdAttachMoney,
   MdInventory2,
